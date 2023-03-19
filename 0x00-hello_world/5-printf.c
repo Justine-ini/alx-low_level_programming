14f993cc4f13317e1f04d2b7f3746761d9742a43
Ini_Justine <inijustine4040@gmail.com>
2023-03-19 11:45:11 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A c programme tha prints a string
+ *
+ * Return: 0
+ * Description:  Write a program using printf to print a series of atrings
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
