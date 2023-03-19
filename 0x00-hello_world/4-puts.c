990c78cf7ec93483b973a25fabe479e8fce1d377
Ini_Justine <inijustine4040@gmail.com>
2023-03-19 11:33:23 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - print a line of code using puts function
+ * main - print a line of code using puts
  *
  * Return: 0
  * Description:  This prints out a statement using puts function in c programming
