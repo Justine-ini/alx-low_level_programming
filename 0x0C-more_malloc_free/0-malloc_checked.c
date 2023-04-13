059690fa80a1d23815e7aee25c1f275ecf498f59
Ini_Justine <inijustine4040@gmail.com>
2023-04-13 16:43:26 -0700

    Updated

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,14 +9,12 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	void *p;
 
-	ptr = malloc(sizeof(ptr) * b)
-
-	if (ptr == NULL)
+	p = malloc(b);
+	if (p == NULL)
 	{
 		exit(98);
 	}
-	return (ptr);
-
+	return (p);
 }
