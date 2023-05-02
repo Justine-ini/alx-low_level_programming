cbb26085a8135da5d23ffcd72eb79ce62ffeca9d
Justine-ini <inijustine4040@gmail.com>
2023-05-02 12:08:21 -0700

    Updated function that frees alinked  list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,19 +8,17 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
-	listint_t *perm;
+	listint_t *current;
 
 	if (head == NULL)
-	{
 		return;
-	}
-	temp = *head;
-	while (temp != NULL)
+
+	while (*head)
 	{
-		perm = temp->next;
-		free (temp);
-		temp = perm;
+		current = (*head)->next;
+		free(*head);
+		*head = current;
 	}
-	*head = NULL;
+
+	head = NULL;
 }
