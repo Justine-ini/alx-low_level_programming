69c7710c5011794e2623eea66eac7e2ee9d86933
Justine-Ini <inijustine4040@gmail.com>
2023-06-09 19:13:39 -0700

    Changes made to file

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -6,11 +6,8 @@
  * main - Prints the minimum number of coins for an amount of money.
  * @argc: The number of command-line arguments.
  * @argv: An array of strings representing the command-line arguments.
- * argv[0] should be the program name and argv[1] should
- * be the amount of money.
- * Return: 0 on success, 1 if the amount is not given or
- * there are too many arguments
- * or prints "0" if the amount is negative.
+ * argv[0] should be the program name and argv[1]the money.
+ * Return: 0 on success, 1 if the amount is not given
  */
 int main(int argc, char *argv[])
 {
@@ -20,6 +17,11 @@ int main(int argc, char *argv[])
 	{
 		printf("Error\n");
 		return (1);
+	}
+	if (argv[1][0] == '-')
+	{
+		printf("0\n");
+		return (coins);
 	}
 	n = atoi(argv[1]);
 	coins = calculateCoins(n);
@@ -31,18 +33,11 @@ int main(int argc, char *argv[])
  * calculateCoins - Calculates the minimum number of
  * coins for a given amount.
  * @n: The amount of money.
- * Return: The minimum number of coins needed
- * or prints "0" if the amount is negative.
+ * Return: 0 if the amount is negative.
  */
 int calculateCoins(int n)
 {
 	int coins = 0;
-
-	if (n < 0)
-	{
-		printf("0\n");
-		return (coins);
-	}
 
 	coins += n / 25;
 	n = n % 25;
