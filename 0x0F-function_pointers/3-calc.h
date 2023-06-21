7ce5660120bd43d6aa156456bd6d173c3a849072
Justine-Ini <inijustine4040@gmail.com>
2023-06-21 10:41:26 -0700

    Task 3 updated

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,10 +1,5 @@
 #ifndef _CALC_H_
 #define _CALC_H_
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,12 +1,13 @@
-#include "3-calc.h"
-#include <stdio.h>
-#include <string.h>
-
+#include"3-calc.h"
 /**
- * get_op_func - Compares function with pointer and returns the equal.
- * @s: points a char
- * Return: Returns the function and the operator when equal
-**/
+*  * get_op_func - get the operator and redirect to the func.
+*   * @s: the operator
+*   (* a blank line
+*   * Description: get the operator and redirect to the func.)?
+*   (* section header: 3-calc.h)*
+*   * Return: return null or the function to do.
+*/
+
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
@@ -17,14 +18,16 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-
 	int i;
 
-	for (i = 0; ops[i].op != NULL; i++)
+	i = 0;
+	while (i < 5)
 	{
-		if (!strcmp(ops[i].op, s))
-			return (ops[i].f);
+		if (strcmp(s, ops[i].op) == 0)
+		{
+			return (*(ops[i]).f);
+		}
+		i++;
 	}
-
 	return (NULL);
 }
