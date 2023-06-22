2f7d20f807dcf86e7b5949fee72ac3b37a0539f0
Justine-Ini <inijustine4040@gmail.com>
2023-06-22 12:02:21 -0700

    Task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - Prints a variable number of integers
+ * separated by a given separator
+ * @separator: The string used to separate the
+ * numbers (or an empty string if NULL)
+ * @n: The number of integers to be printed
+ * @...: The list of integers to be printed
+ */
+void print_numbers(const char *separator, const unsigned int n, ...);
+{
+	unsigned int i = 0;
+	va_list arg_list;
+
+	va_start(arg_list, n);
+
+	if (separator == NULL)
+	{
+		separator = "";
+	}
+
+	while (i < n)
+	{
+		printf("%d", va_arg(arg_list, int));
+
+		if (i < n - 1)
+		{
+			printf("%s", separator);
+		}
+		i++;
+	}
+	printf("\n");
+}
