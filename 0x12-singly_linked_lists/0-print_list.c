3d419c266b04d3a57ffc127ee880497d90ca95a3
Justine-Ini <inijustine4040@gmail.com>
2023-07-03 14:18:08 -0700

    Bug fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,21 +9,20 @@
 
 size_t print_list(const list_t *h)
 {
-	const list_t *current = h;
 	size_t count = 0;
 
-	while (current != NULL)
+	while (h != NULL)
 	{
-		if (current->str != NULL)
+		if (h->str != NULL)
 		{
-			printf("[%d]%s\n", current->len, current->str);
+			printf("[%u]%s\n", h->len, h->str);
 		}
 		else
 		{
 			printf("[0] (nil)\n");
 		}
 		count++;
-		current = current->next;
+		h = h->next;
 	}
 	return (count);
 }
