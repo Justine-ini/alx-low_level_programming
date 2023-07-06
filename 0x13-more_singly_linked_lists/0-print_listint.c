2e2b7315f5e4473b9cb0e65ac2fba2a3c5f63c4c
Justine-Ini <inijustine4040@gmail.com>
2023-07-06 11:58:45 -0700

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,7 +2,7 @@
 
 /**
  * print_listint - Prints all the elements of a listint_t list.
- * @h: A pointer to the head of the listint_t list.
+ * @h: A pointer to the head of the listint_t list
  *
  * Return: The number of nodes in the list.
  */
