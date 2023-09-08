ca179d3f898a62f59bc0fdbc79f57881b41a155f
Justine-ini <inijustine4040@gmail.com>
2023-09-08 08:40:09 -0700

    Bugfix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,7 +14,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (size == 0)
 		return (NULL);
 
-	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+	new_table = malloc(sizeof(hash_table_t));
 
 	if (new_table == NULL)
 		return (NULL);
@@ -24,9 +24,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (new_table->array == NULL)
 	{
-		free(new_table);
 		return (NULL);
 	}
 
 	return (new_table);
-}
