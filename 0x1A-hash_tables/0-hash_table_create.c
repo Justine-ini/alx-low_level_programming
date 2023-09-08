a676528330738a45a5baa3bc041ef8a74d55992e
Justine-ini <inijustine4040@gmail.com>
2023-09-08 08:34:55 -0700

    Bugfix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -20,7 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	new_table->size = size;
-	new_table->array = calloc(size, sizeof(hash_node_t *) * size);
+	new_table->array = malloc(size, sizeof(hash_node_t *) * size);
 
 	if (new_table->array == NULL)
 	{
